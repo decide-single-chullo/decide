0e2aad9de002c356df45a3fc05ee3c0870c10bb0
Enrique González Boza <enrgonboz@alum.us.es>
2022-01-07 21:21:16 +0100

    feature: Implementar tests de Selenium
    Se han implementado tests de Selenium para  autenticarse utilizando el email y contraseña. Issue #66

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -7,6 +7,14 @@ from rest_framework.authtoken.models import Token
 
 from base import mods
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from base.tests import BaseTestCase
+
+import time 
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class AuthTestCase(APITestCase):
 
@@ -26,7 +34,7 @@ class AuthTestCase(APITestCase):
 
     def tearDown(self):
         self.client = None
-
+    
     def test_login(self):
         data = {'username': 'voter1', 'password': '123'}
         response = self.client.post('/authentication/login/', data, format='json')
@@ -158,4 +166,35 @@ class AuthTestCase(APITestCase):
         response = self.client.post('/authentication/login/', data, format='json')
         self.assertEqual(response.status_code, 400)
 
+#   Test view with selenium
 
+class AuthenticationTestCase(StaticLiveServerTestCase):
+    
+    def setUp(self):
+        #Load base test functionality for decide
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        self.driver = webdriver.Chrome(options=options)
+
+        super().setUp()    
+
+    def tearDown(self):
+        super().tearDown()
+        self.driver.quit()
+        self.base.tearDown()
+
+#   Test selenium to authenticate using email. Issue 33.
+
+    def test_authentication_using_email(self):
+        base_url = f'{self.live_server_url}'
+
+        self.driver.get(base_url + '/admin/logout')
+        self.driver.get(base_url + '/admin/logout/?next=/admin/')
+        self.driver.find_element(By.ID, "id_username").send_keys("admin@email.com")
+        self.driver.find_element(By.ID, "id_password").send_keys("qwerty")
+        self.driver.find_element(By.ID, "id_password").send_keys(Keys.ENTER)
+        assert self.driver.page_source.__contains__("Site administration")
+        
